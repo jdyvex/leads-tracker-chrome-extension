059d7932d625c5f1faf4f8eaf7b22c58b03f867a
jdyvex <jordandyvex@gmail.com>
2023-03-24 13:39:36 -0400

    leads-tracker-chrome-extension

---
+++ README.md
@@ -0,0 +1,3 @@
+# Leads Tracker
+
+Leads Tracker is a simple web application to help businesses manage and track their leads effectively. It allows users to add and delete leads, as well as save their current tab as a lead, providing an easy-to-use interface for efficient lead management.
---
+++ index.js
@@ -0,0 +1,69 @@
+const inputBtn = document.getElementById("input-btn");
+const tabBtn = document.getElementById("tab-btn");
+const deleteBtn = document.getElementById("delete-btn");
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
+
+let myLeads = [];
+
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
+
+function isValidUrl(url) {
+  try {
+    new URL(url);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    const tabUrl = tabs[0].url;
+    if (isValidUrl(tabUrl)) {
+      myLeads.push(tabUrl);
+      localStorage.setItem("myLeads", JSON.stringify(myLeads));
+      render(myLeads);
+    } else {
+      alert("Invalid URL");
+    }
+  });
+});
+
+function render(leads) {
+  let listItems = "";
+  for (i = 0; i < leads.length; i++) {
+    listItems += `
+        <li>
+            <a target='_blank' href='${leads[i]}'>
+            ${leads[i]}
+        </li>
+    `;
+  }
+  ulEl.innerHTML = listItems;
+}
+
+inputBtn.addEventListener("click", function () {
+  const inputValue = inputEl.value.trim();
+  if (isValidUrl(inputValue)) {
+    myLeads.push(inputValue);
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  } else {
+    myLeads.push("https://" + inputValue);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  }
+});
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
